fd7cdcbfb843968093477ee7c39b94c88e3bffc8
Koupah <ktayl21@yahoo.com.au>
2020-12-24 21:22:53 +1000

    Cleaned up

--- Valorant-Ranked-Checker/src/club/koupah/valorant/ranked/RankedChecker.java
+++ Valorant-Ranked-Checker/src/club/koupah/valorant/ranked/RankedChecker.java
@@ -1,21 +1,11 @@
 package club.koupah.valorant.ranked;
 
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Map;
-
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-
-import me.tewpingz.valorant.ValorantAPI;
 import me.tewpingz.valorant.auth.ValAuthentication;
 import me.tewpingz.valorant.auth.ValRegion;
 
